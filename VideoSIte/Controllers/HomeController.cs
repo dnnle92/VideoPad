6201cba61c6b4b156ef5af343ac5b7159b4c7625
Dan Le <dnnle92@outlook.com>
2019-07-26 17:27:45 -0400

    Initial commit with video  and EF map table auto created migration

--- VideoSIte/Controllers/HomeController.cs
+++ VideoSIte/Controllers/HomeController.cs
@@ -10,31 +10,8 @@ namespace VideoSIte.Controllers
 {
     public class HomeController : Controller
     {
-        private ApplicationDbContext context = new ApplicationDbContext();
-
         public ActionResult Index()
         {
-            return View();
-        }
-        public ActionResult Video(int id)
-        {
-
-
-            Video video = new Video()
-            {
-                VideoName = "https://www.youtube.com/watch?v=9TBTPf_TiPk",
-                DateAdded = DateTime.Now
-
-            };
-            VideoCategory videoCategory = new VideoCategory()
-            {
-                CatName = "Sports"
-            };
-
-            context.Videos.Add(video);
-            context.VideoCategories.Add(videoCategory);
-            context.SaveChanges();
-
             return View();
         }
 
---
+++ VideoSIte/Controllers/VideoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoSIte.Models;
+
+namespace VideoSIte.Controllers
+{
+    public class VideoController : Controller
+    {
+        private ApplicationDbContext context = new ApplicationDbContext();
+        var video = context
+        // GET: Video
+        public ActionResult Index()
+        {
+            Video video = new Video()
+            {
+                VideoName = "TestVideo",
+                DateAdded = DateTime.Now
+            };
+            context.Videos.Add(video);
+            context.SaveChanges();
+            return View();
+        }
+    }
+}
