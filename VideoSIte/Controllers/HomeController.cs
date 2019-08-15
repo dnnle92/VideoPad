0eda98d8ad036aa9fc7a4a37c4d10ca1020366de
Dan Le <dnnle92@outlook.com>
2019-08-14 22:43:58 -0400

    Added User side VideosController and its views

--- VideoSIte/Controllers/HomeController.cs
+++ VideoSIte/Controllers/HomeController.cs
@@ -18,26 +18,6 @@ namespace VideoSIte.Controllers
             return View();
         }
 
-        public PartialViewResult CategoryPartial()
-        {
-            var categoryList = db.Categories.OrderBy(x => x.CatName).ToList();
-            return PartialView(categoryList);
-        }
-
-        public PartialViewResult VideoListPartial(int? category)
-        {
-            if (category != null)
-            {
-                var videosList = db.Videos.OrderByDescending(a => a.VideoId).Where(a => a.Categories.Any(c => c.CatId == category)).ToList();
-                return PartialView(videosList);
-            }
-            else
-            {
-                var videosList = db.Videos.OrderByDescending(a => a.VideoId).ToList();
-                return PartialView(videosList);
-            }
-        }
-
             public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ VideoSIte/Controllers/VideosController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VideoSIte.Models;
+
+namespace VideoSIte.Controllers
+{
+    public class VideosController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Videos
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult CategoryPartial()
+        {
+            var categoryList = db.Categories.OrderBy(x => x.CatName).ToList();
+            return PartialView(categoryList);
+        }
+
+        public PartialViewResult VideoListPartial(int? category)
+        {
+            if (category != null)
+            {
+                var videosList = db.Videos.OrderByDescending(a => a.VideoId).Where(a => a.Categories.Any(c => c.CatId == category)).ToList();
+                return PartialView(videosList);
+            }
+            else
+            {
+                var videosList = db.Videos.OrderByDescending(a => a.VideoId).ToList();
+                return PartialView(videosList);
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Video video = db.Videos.Find(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+    }
+}
